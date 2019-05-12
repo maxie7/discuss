6d513ec2e3a0a669de32afeba1b02ac29af4bf83
MaxKos <maxie7@protonmail.com>
2019-05-12 22:17:20 +0200

    Add >> messages to sockets

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,11 +1,15 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
   
-  def join() do
-    
+  def join(name, _params, socket) do
+    {:ok, %{hey: "there"}, socket}
   end
 
-  def handle_in() do
-    
+  def handle_in(name, message, socket) do
+    IO.puts("+++++")
+    IO.puts(name)
+    IO.inspect(message)
+
+    {:reply, :ok, socket}
   end
 end
