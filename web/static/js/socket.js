d2fd814e765df7bb5226861a5422d1192b8dd84a
MaxKos <maxie7@protonmail.com>
2019-06-10 23:42:18 +0200

    Add Broadcasting Updates && Add Updating the Client && Fix 'undefined' Socket Event Object

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -5,13 +5,16 @@ socket.connect()
 
 const createSocket = (topicId) =>  {
   let channel = socket.channel(`comments:${topicId}`, {})
-  channel.join()
-    .receive("ok", resp => {
-      renderComments(resp.comments);
-    })
-    .receive("error", resp => {
-      console.log("Unable to join", resp)
-    });
+  channel
+      .join()
+      .receive("ok", resp => {
+        renderComments(resp.comments);
+      })
+      .receive("error", resp => {
+        console.log("Unable to join", resp)
+      });
+
+  channel.on(`comments:${topicId}:new`, renderComment);
 
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value;
@@ -22,14 +25,24 @@ const createSocket = (topicId) =>  {
 
 function renderComments(comments) {
   const renderedComments = comments.map(comment => {
-    return `
-      <li class="collection-item">
-        ${comment.content}
-      </li>
-    `;
+    return commentTemplate(comment);
   });
 
   document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
+
+  document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+function commentTemplate(comment) {
+  return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+  `;
 }
 
 window.createSocket = createSocket;
