e263c0194e0fc9e1e6239ce79f3cdd6039082c5d
MaxKos <maxie7@protonmail.com>
2019-06-11 23:17:35 +0200

    Verify the current user socket && assoc comments with the users

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,12 @@ defmodule Discuss.UserSocket do
   transport :websocket, Phoenix.Transports.WebSocket
 
   def connect(%{"token" => token }, socket) do
-    {:ok, socket}
+    case Phoenix.Token.verify(socket, "key", token) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _error} ->
+        :error
+    end
   end
 
   def id(_socket), do: nil
