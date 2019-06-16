0a803c5cb0b0d5a7315b7bf4a66e38e84ee70415
MaxKos <maxie7@protonmail.com>
2019-06-16 14:39:21 +0200

    Fetch users with Comments && Render associated Users

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -8,6 +8,7 @@ const createSocket = (topicId) =>  {
   channel
       .join()
       .receive("ok", resp => {
+        console.log(resp);
         renderComments(resp.comments);
       })
       .receive("error", resp => {
@@ -38,9 +39,17 @@ function renderComment(event) {
 }
 
 function commentTemplate(comment) {
+  let email = 'Anonymous';
+  if (comment.user) {
+      email = comment.user.email;
+  }
+
   return `
       <li class="collection-item">
         ${comment.content}
+        <div class="secondary-content">
+          ${email}
+        </div>  
       </li>
   `;
 }
